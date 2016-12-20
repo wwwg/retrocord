c5d0a4df99afe85a5b74091f1c7a59ba5502585d
Gus Caplan <me@gus.host>
2016-12-20 15:27:48 -0600

    fix a small issue

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const localStorage = new LocalStorage(path.join(__dirname, 'cache'));
 
 const lp = (v, n, c = '0') => String(v).length >= n ? `${v}` : (String(c).repeat(n) + v).slice(-n);
 
-const timestamp = (d = new Date(), mdy = false) =>
+const timestamp = vorpal.timestamp = (d = new Date(), mdy = false) =>
   `${mdy ? `${lp(d.getFullYear().toString(), 2)}-${lp((d.getMonth() + 1).toString(), 2)}-${lp(d.getDate().toString(), 2)} ` : ''}
 ${lp(d.getHours().toString(), 2)}:
 ${lp(d.getMinutes().toString(), 2)}:
