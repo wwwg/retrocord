80281fd6e60bb2719467e30e503c8f2652616746
Nomsy <bpalmer647@gmail.com>
2017-07-27 15:26:33 -0400

    Message Handler additions (#14)
    * perfect-o! additions added
    
    * perfect-o! dms, groups, and mentions added
    
    * Undo changes in commands.js
    
    * removed newlines from discord/index.js

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const Storage = require('../Storage');
+const timestamp = require('../util/timestamp');
 
 const client = new Discord.Client({
   // some options
@@ -19,8 +20,27 @@ function run(ctx) {
   });
 
   client.on('message', (message) => {
-    if (message.channel !== ctx.current.channel) return;
-    ctx.gui.putMessage(message);
+    switch (message.channel.type) {
+      case 'dm': {
+        if (message.author.id === client.user.id) return;
+        ctx.gui.put(`{yellow-fg}${timestamp(message.createdAt, false)}{/yellow-fg} {bold}${message.author.tag}{/bold} has sent you a message!`);
+        break;
+      }
+      case 'group': {
+        if (message.author.id === client.user.id) return;
+        let dmName = message.channel.name === null ? 'Group' : message.channel.name;
+        ctx.gui.put(`{yellow-fg}${timestamp(message.createdAt, false)}{/yellow-fg} {bold}${dmName} has a new message!`);
+        break;
+      }
+      case 'text': {
+        if (message.channel === ctx.current.channel) {
+          ctx.gui.putMessage(message);
+        } else if (message.mentions.users.has(client.user.id)) {
+          ctx.gui.put(`{yellow-fg}${timestamp(message.createdAt, false)}{/yellow-fg} You were mentioned in {bold}${message.guild.name} #${message.channel.name}{/bold} by {bold}${message.author.tag}{/bold}`);
+        }
+        break;
+      }
+    }
   });
 
   client.login(Storage.get('token'))
