f71140a622d7bb75b3132bea4d7542f6349616d0
Gus Caplan <me@gus.host>
2017-07-27 16:37:20 -0500

    fall back to jimp

--- src/elements/image.js
+++ src/elements/image.js
@@ -1,17 +1,38 @@
 const request = require('snekfetch');
-const sharp = require('sharp');
 const tng = require('blessed/vendor/tng');
 const Storage = require('../Storage');
+
+const WIDTH_TARGET = 32;
+
+let resize;
+try {
+  const sharp = require('sharp');
+  resize = (data, width, height) => {
+    const factor = width / WIDTH_TARGET;
+    return sharp(data)
+      .resize(WIDTH_TARGET, Math.round(height / factor))
+      .toBuffer();
+  };
+} catch (e) {
+  const jimp = require('jimp');
+  resize = (data, width, height) =>
+    new Promise((resolve, reject) => {
+      const factor = width / WIDTH_TARGET;
+      jimp.read(data, (error, image) => {
+        if (error) return reject(error);
+        image.resize(WIDTH_TARGET, Math.round(height / factor))
+          .getBuffer(jimp.AUTO, (err, buffer) => {
+            if (err) reject(err);
+            else resolve(buffer);
+          });
+      });
+    });
+}
 
 async function imageElement({ id, url, width, height }) {
   if (await Storage.hasFile(id)) return makeImage({ path: Storage.getFilePath(id), width, height });
   const data = await request.get(url)
-    .then((r) => sharp(r.body))
-    .then((s) => {
-      const target = 32;
-      const factor = width / target;
-      return s.resize(target, Math.round(height / factor)).toBuffer();
-    });
+    .then((r) => resize(r.body, width, height));
   await Storage.setFile(id, data);
   return makeImage({ path: Storage.getFilePath(id), width, height });
 }
