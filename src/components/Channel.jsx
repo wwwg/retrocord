b6ad43fb563f11aa8b81581debe84e2e0da88910
Gus Caplan <me@gus.host>
2017-08-02 12:31:45 -0500

    eh

--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -1,25 +1,27 @@
 const React = require('react');
 
-const ctx = require('../ctx');
-
 const MessageComponent = require('./Message.jsx');
+
+const ctx = require('../ctx');
+const discord = require('../discord');
 
 class Channel extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { messages: [] };
+
+    discord.on('message', ({ channel }) => {
+      if (channel === ctx.current.channel) this.updateMessages();
+    });
   }
 
   render() {
     return (
-      <box tags={true}
-        scrollable={true}
-        alwaysScroll={true}
-        mouse={true}
-        scrollbar={{ ch: '', inverse: true }}>
+      <list interactive={false}
+        mouse={true}>
         {this.state.messages.map((message) => <MessageComponent key={message.id} message={message}/>)}
-      </box>
+      </list>
     );
   }
 
