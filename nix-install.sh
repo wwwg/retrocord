02fa4032a6644fa78f7bd2d9ab68b874799b834a
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-08-26 13:25:48 -0500

    Add install script

---
+++ nix-install.sh
@@ -0,0 +1,10 @@
+# Download and install required files
+git clone https://github.com/wwwg/retrocord.git
+cd retrocord
+npm i
+# Lint and build
+npm run lint
+npm run build
+# Run
+echo "retrocord is installed. running.."
+node src/index.js
