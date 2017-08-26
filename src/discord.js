28c273792e650238a82bb1ddc81f660f65c987c1
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-08-26 14:45:27 -0500

    Login error message

--- src/discord.js
+++ src/discord.js
@@ -13,7 +13,8 @@ client.run = ctx => {
 
   client.login(ctx.token)
     .catch(e => {
-      ctx.gui.put(`{bold}Login error{/bold} (${e.message})`);
+      ctx.gui.put(`{bold}Login error:{/bold} ${e.message}.`);
+      ctx.gui.put('Try using the :login <token> command to log in.');
     });
 }
 module.exports = client;
