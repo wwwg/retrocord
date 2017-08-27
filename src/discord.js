b6ee4580ac215bc79d47b13b9112601e4e132571
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-08-27 11:44:16 -0500

    Login command formatting

--- src/discord.js
+++ src/discord.js
@@ -6,7 +6,7 @@ client.run = ctx => {
     if (client.user.bot) {
       client.destroy();
     } else {
-      ctx.gui.put(`{bold}Logged in as ${client.user.tag}{/bold}`);
+      ctx.gui.put(`{bold}Logged in as {yellow-fg}${client.user.tag}{/yellow-fg}{/bold}`);
       ctx.gui.put('Use the join command to join a guild, dm, or channel (:join discord api #general)');
     }
   });
