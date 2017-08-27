f7735d0081f24b9523a457c0ae98d476f740048f
wwwg <xxperipsis@gmail.com>
2017-08-27 13:44:12 -0500

    Update *nix global installer

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-mkdir -p /usr/share/retrocord-light
-cp -rf src/ /usr/share/retrocord-light
-echo "node /usr/share/retrocord-light/src/index.js" > /usr/bin/retrocord-light
-chmod +x /usr/bin/retrocord-light
+sudo mkdir -p /usr/share/retrocord-light
+sudo cp -rf src/ /usr/share/retrocord-light
+npm i
+sudo cp -rf node_modules/ /usr/share/retrocord-light/node_modules/
+sudo sh -c "echo \"node /usr/share/retrocord-light/src/index.js\" > /usr/bin/retrocord-light"
+sudo chmod +x /usr/bin/retrocord-light
