f4fbc3c85e7c7fa2ceaa1ef0c58139362f0fc341
wwwg <xxperipsis@gmail.com>
2017-08-27 13:44:44 -0500

    Change NPM usage in *nix installation

--- nix-install.sh
+++ nix-install.sh
@@ -1,7 +1,6 @@
 # Download and install required files
 git clone https://github.com/wwwg/retrocord-light.git
 cd retrocord-light
-sudo npm -i -g i
 # install
 npm run install-nix
 echo -n "Installed Retrocord light."
