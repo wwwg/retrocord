027064b5e967d044e9825b5fd2f12bd64a5ae583
wwwg <xxperipsis@gmail.com>
2017-08-29 12:34:25 -0500

    change arrow directions

--- src/gui.js
+++ src/gui.js
@@ -160,7 +160,7 @@ class GUI extends EventEmitter {
     const me = this;
     messages.forEach(m => {
       const ts = timestamp(m.createdAt),
-        arrow = (m.author.id == ctx.discord.user.id) ? `{green-fg}{bold}>{/bold}{/green-fg}` : `{red-fg}{bold}<{/bold}{/red-fg}`,
+        arrow = (m.author.id == ctx.discord.user.id) ? `{green-fg}{bold}<{/bold}{/green-fg}` : `{red-fg}{bold}>{/bold}{/red-fg}`,
         txt = `${arrow} {yellow-fg}{bold}${ts}{/bold}{/yellow-fg} {white-fg}{bold}${m.author.username}{/bold}{/white-fg} ${m.content}`;
       me.put(txt);
     });
