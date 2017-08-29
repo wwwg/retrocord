a566eef114ffb84c1d82c0d57b59b038fe05fd07
wwwg <xxperipsis@gmail.com>
2017-08-29 11:58:14 -0500

    Improve text formatting

--- src/gui.js
+++ src/gui.js
@@ -90,7 +90,7 @@ class GUI extends EventEmitter {
         if (ctx.dmIsTyping) {
           txt += `\n{center}{underline}{white-fg}is typing...{/white-fg}{/underline}{/center}`;
         }
-        txt += `\n{center}Status: {bold}${ctx.current.channel.recipient.presence.status}{/bold}{/center}`;
+        txt += `\n{underline}Status: {bold}${ctx.current.channel.recipient.presence.status}{/underline}{/center}\n`;
       }
       if (!ctx.hideFriends) {
         txt += "\nFriends:";
